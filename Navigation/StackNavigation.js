5f4889f4128e414a0e0b81df19fd1142f35abddd
Daniel <daniel.munoz.me1@gmail.com>
2024-03-22 05:06:37 -0700

    using google sign in

--- Navigation/StackNavigation.js
+++ Navigation/StackNavigation.js
@@ -7,9 +7,35 @@ import { UserProfile } from "../Components/UserProfile";
 import { UserList } from "../Components/UserList";
 import { RoleList } from "../Forms/CommunityRole";
 
+import * as React from "react";
+import { Button } from "react-native";
+import * as WebBrowser from "expo-web-browser";
+import * as Google from "expo-auth-session/providers/google";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const ANDROID_CLIENT_ID =
+  "529268756415-rkptqemo4r09g2inubdssqoblnuvmctd.apps.googleusercontent.com";
+const IOS_CLIENT_ID =
+  "529268756415-7ne0nuqa2cp19uobhkjos5b7agkelr5n.apps.googleusercontent.com";
+const WEB_CLIENT_ID =
+  "529268756415-5uclr7f3r173o870k4h0stqmj81fp3b5.apps.googleusercontent.com";
+
+WebBrowser.maybeCompleteAuthSession();
+
 const Stack = createNativeStackNavigator();
 
 export const MyStack = () => {
+  const [userInfo, setUserInfo] = React.useState(null);
+
+  //client IDs from .env
+  const config = {
+    androidClientId: ANDROID_CLIENT_ID,
+    iosClientId: IOS_CLIENT_ID,
+    webClientId: WEB_CLIENT_ID,
+  };
+
+  const [request, response, promptAsync] = Google.useAuthRequest(config);
+
   return (
     <NavigationContainer>
       <Stack.Navigator
@@ -21,6 +47,14 @@ export const MyStack = () => {
           headerTitleStyle: {
             fontWeight: "bold",
           },
+          headerRight: () => (
+            <Button
+              title="sign in with google"
+              onPress={() => {
+                promptAsync();
+              }}
+            />
+          ),
         }}
       >
         <Stack.Screen
